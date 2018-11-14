49db3decca8e4d7898abdc81e7d2a7cdc23c42d4
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-11-14 14:40:04 -0600

    Updates

---
+++ precache-manifest.d7e564f47fa266c5b77415966be547a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8789f39d442855b28d85",
+    "url": "/newMovies/static/js/runtime~main.8789f39d.js"
+  },
+  {
+    "revision": "0a2c932eb42cd7e10037",
+    "url": "/newMovies/static/js/main.0a2c932e.chunk.js"
+  },
+  {
+    "revision": "d98feb71996439767f10",
+    "url": "/newMovies/static/js/1.d98feb71.chunk.js"
+  },
+  {
+    "revision": "0a2c932eb42cd7e10037",
+    "url": "/newMovies/static/css/main.8d1debce.chunk.css"
+  },
+  {
+    "revision": "f20c1e496aa18a8b98e65bc40045f61b",
+    "url": "/newMovies/index.html"
+  }
+];
