a864f1b1a2e1f712c4f4cbd9c3648a55ff3e8d19
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-11-18 09:43:47 -0600

    Fixed Submit Functionality

--- src/Components/Nav/Search.js
+++ src/Components/Nav/Search.js
@@ -12,24 +12,31 @@ const Search = props => {
       }}
       className="searchBar"
     >
-      <form onChange={props.change} onSubmit={props.onChange}>
+      <form onChange={props.change}>
         <input type="text" placeholder="Type Movie or Show Name" />
+
+        <Link
+          to={{
+            pathname: `/search/${props.results}`,
+            state: {
+              search: props.results
+            }
+          }}
+          style={{ height: "25px", marginLeft: "10px" }}
+        >
+          {" "}
+          <button
+            type="submit"
+            style={{ height: 0, width: 0, padding: 0, border: 0 }}
+          />
+          <img
+            src="https://img.icons8.com/windows/50/ffffff/search.png"
+            style={{ height: "25px", color: "#fff" }}
+            alt="search"
+            type="submit"
+          />
+        </Link>
       </form>
-      <Link
-        to={{
-          pathname: `/search/${props.results}`,
-          state: {
-            search: props.results
-          }
-        }}
-        style={{ height: "25px", marginLeft: "10px" }}
-      >
-        <img
-          src="https://img.icons8.com/windows/50/ffffff/search.png"
-          style={{ height: "25px", color: "#fff" }}
-          alt="search"
-        />
-      </Link>
     </div>
   );
 };
